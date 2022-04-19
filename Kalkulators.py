2544e5f00ebc50d7b06a9a1d6a28ddf825584a41
Pundins <mpundinsyt@gmail.com>
2022-04-19 09:34:46 +0300

    strādā vienāds

--- Kalkulators.py
+++ Kalkulators.py
@@ -2,6 +2,31 @@ from tkinter import*
 mansLogs=Tk()
 mansLogs.title("Kalkulators")
 #mansLogs.geometry("300x450")
+
+def vienads():
+    global num2
+    num2=(int(e.get()))
+    result=0
+    if mathOp=="+":
+        result=num1+num2
+    elif mathOp=="-":
+        result=num1-num2
+    elif mathOp=="x":
+        result=num1*num2
+    elif mathOp=="/":
+        result=num1/num2
+    else:
+        result=0
+    e.delete(0, END)
+    e.insert(0, str(result))
+    return 0
+
+def notirit():
+    e.delete(0, END)
+    num1=0
+    mathOp=""
+    return 0 
+
 btn0 = Button(mansLogs, text="0", padx="40", pady="30", command=lambda:btnClick(0))
 btn1 = Button(mansLogs, text="1", padx="40", pady="30", command=lambda:btnClick(1))
 btn2 = Button(mansLogs, text="2", padx="40", pady="30", command=lambda:btnClick(2))
@@ -16,8 +41,8 @@ btn10 = Button(mansLogs, text="-", padx="40", pady="30", command=lambda:btnComma
 btn11 = Button(mansLogs, text="+", padx="40", pady="30", command=lambda:btnCommand("+"))
 btn12 = Button(mansLogs, text="/", padx="40", pady="30", command=lambda:btnCommand("/"))
 btn13 = Button(mansLogs, text="x", padx="40", pady="30", command=lambda:btnCommand("x"))
-btn14 = Button(mansLogs, text="=", padx="40", pady="30", command=lambda:btnCommand("="))
-btn15 = Button(mansLogs, text="C", padx="40", pady="30", command=lambda:btnCommand("C"))
+btn14 = Button(mansLogs, text="=", padx="40", pady="30", command=vienads)
+btn15 = Button(mansLogs, text="C", padx="40", pady="30", command=notirit)
 btn16 = Button(mansLogs, text=".", padx="40", pady="30", command=lambda:btnClick("."))
 
 btn0.grid(row=4,column=1)
@@ -48,16 +73,13 @@ def btnClick(number):
 def btnCommand(command):
     global num1 # jāiegaumē skaitlis un darbība
     global mathOp # matemātisks operators
-    MathOp = command # +,-,*,/
+    mathOp = command # +,-,*,/
     num1 = float(e.get())
     e.delete(0, END)
     return 0
 
 e=Entry(mansLogs, width=15, font=("Arial Black", 20))
 e.grid(row=0, column=0, columnspan=5)
-
-
-
 
 #poga.pack()
 
